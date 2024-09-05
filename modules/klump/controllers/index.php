7d5cf86a907ea8381aaa609388c7b0f43216a003
Celestine Omin <celestineomin@gmail.com>
2024-09-05 21:59:45 +0100

    Added index to the controller folder

--- modules/klump/controllers/index.php
+++ modules/klump/controllers/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyyright since 2024 Klump Inc. and Contributors
+ * 
+ * @author Klump Inc <engineering@useklump.com>
+ * @copyright 2024 Klump Inc. and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
